2d6c4018f04f3e0f22fada225a2f3ab0f06ad78e
jimmy <skawk3235@gmail.com>
2025-03-08 21:07:44 +0900

    productFilter 추가

---
+++ angularjs_app/filters/customFilters.js
@@ -0,0 +1,18 @@
+angular.module("customFilters", []).filter("unique", function () {
+  return function (data, propertyName) {
+    if (angular.isArray(data) && angular.isString(propertyName)) {
+      let results = [];
+      let keys = {};
+      for (var i = 0; i < data.length; i++) {
+        let val = data[i][propertyName];
+        if (angular.isUndefined(keys[val])) {
+          keys[val] = true;
+          results.push(val);
+        }
+      }
+      return results;
+    } else {
+      return data;
+    }
+  };
+});
